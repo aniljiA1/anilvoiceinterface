2f4f2ff9acc2dee4617cf779acabf5b4ea04e208
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2025-08-26 22:08:34 +0530

    vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://anilvoiceinterface-1.onrender.com",
         changeOrigin: true,
       },
     },
